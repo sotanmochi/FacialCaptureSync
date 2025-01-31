1f91def5bcd1bb42652c954cebbef193a8fdf9bb
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2025-01-31 14:32:25 +0900

    Refactor FacialCaptureReceiver to simplify data handling and remove unused buffer logic

--- src/FacialCaptureSync/Packages/FacialCaptureSync/Runtime/FacialCaptureReceiver.cs
+++ src/FacialCaptureSync/Packages/FacialCaptureSync/Runtime/FacialCaptureReceiver.cs
@@ -16,38 +16,22 @@ namespace FacialCaptureSync
     /// </summary>
     public sealed class FacialCaptureReceiver : IDisposable
     {
-        public bool IsRunning => _isRunning;
-        
         private readonly IFacialCaptureSource _captureSource;
-        
+
+        private FacialCapture _receivedDataBuffer = new();
+
         private bool _isRunning = false;
         private CancellationTokenSource _cancellationTokenSource;
         private UdpClient _udpClient;
         private IPAddress _captureDeviceAddress;
-        
-#region Ring Buffer
-        public int CaptureBufferSize => _bufferSize;
-        
-        private readonly int _bufferSize; // Buffer size must be a power of two.
-        private readonly long _bufferMask;
-        private readonly FacialCapture[] _captureBuffer;
-        
-        private long _bufferHead = 0;
-        private long _bufferTail = 0;
-#endregion
-        
+
+        public event Action<FacialCapture> OnDataReceived;
+
+        public bool IsRunning => _isRunning;
+
         public FacialCaptureReceiver(IFacialCaptureSource captureDataSource, int bufferSize = 4)
         {
             _captureSource = captureDataSource;
-
-            _bufferSize = MathUtils.CeilingPowerOfTwo(bufferSize); // Buffer size must be a power of two.
-            _bufferMask = _bufferSize - 1;
-            
-            _captureBuffer = new FacialCapture[_bufferSize];
-            for (var i = 0; i < _captureBuffer.Length; i++)
-            {
-                _captureBuffer[i] = new FacialCapture();
-            }
         }
         
         public void Dispose()
@@ -55,18 +39,7 @@ namespace FacialCaptureSync
             Disconnect();
             Stop();
         }
-        
-        public bool TryDequeueCapture(ref FacialCapture capture)
-        {
-            if (_bufferHead >= _bufferTail) { return false; }
-            
-            var index = _bufferHead & _bufferMask;
-            _captureBuffer[index].CopyTo(capture);
-            _bufferHead++;
-            
-            return true;
-        }
-        
+
         public void Start()
         {
             if (_isRunning) { return; }
@@ -118,7 +91,6 @@ namespace FacialCaptureSync
         private async void RunReceiverLoop(CancellationToken cancellationToken = default)
         {
             _udpClient = new UdpClient(_captureSource.ReceiverPort);
-            
             while (!cancellationToken.IsCancellationRequested)
             {
                 var result = await _udpClient.ReceiveAsync().ConfigureAwait(false);
@@ -130,16 +102,10 @@ namespace FacialCaptureSync
         {
             try
             {
-                var bufferFreeCount = _bufferSize - (int)(_bufferTail - _bufferHead);
-                if (bufferFreeCount > 0)
+                var message = System.Text.Encoding.ASCII.GetString(data);
+                if (_captureSource.TryParse(message, ref _receivedDataBuffer))
                 {
-                    var message = System.Text.Encoding.ASCII.GetString(data);
-                    var index = _bufferTail & _bufferMask;
-                    
-                    if (_captureSource.TryParse(message, ref _captureBuffer[index]))
-                    {
-                        _bufferTail++;
-                    }
+                    OnDataReceived?.Invoke(_receivedDataBuffer);
                 }
             }
             catch (Exception e)
